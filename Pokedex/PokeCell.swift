fac524912fe23bae0cb8061500d5118511bb5ca0
Steven Santiago <stevensantiago@Stevens-MacBook-Air.local>
2017-01-30 02:59:14 -0800

    Loading all pokemon into collection view by parsing through csv file

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Steven Santiago on 1/29/17.
+//  Copyright © 2017 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell (_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: String(self.pokemon.pokedexId))
+    }
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Steven Santiago on 1/29/17.
+//  Copyright © 2017 Steven Santiago. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
