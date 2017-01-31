3c7a95ce109f625462e71587cf96f2a3f9088893
Steven Santiago <stevensantiago@Stevens-MacBook-Air.local>
2017-01-31 12:44:48 -0800

    Added music along with functional search operation

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var pokemon = [Pokemon]()
-
+    var filteredPokemon = [Pokemon]()
+    var inSearch = false // identifies if someting is being searched
+    var musicPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.dataSource = self
         collection.delegate = self
+        searchBar.delegate = self
+        
+        searchBar.returnKeyType = UIReturnKeyType.done // changes return key to done on keyboard
         
         parsePokemonCSV()
+        initAudio()
     }
-
+    
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1 // continous play
+            musicPlayer.play()
+            
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+    }
+    
     
     func parsePokemonCSV() {
         let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
@@ -43,11 +66,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             print(err.debugDescription)
         }
     }
-
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeIdentifier", for: indexPath) as? PokeCell{
-            let poke = pokemon[indexPath.row]
+            let poke: Pokemon!
+            if inSearch{
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
             cell.configureCell(poke)
             
             return cell
@@ -57,12 +85,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    
+        
     }
-
+    
     //Number of cells
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pokemon.count
+        if inSearch {
+            return filteredPokemon.count
+        } else {
+            return pokemon.count
+        }
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -72,6 +104,33 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     // Size of cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 105, height: 105)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) { // makes keyboard go away  after searching
+        view.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearch = false
+            collection.reloadData()
+            view.endEditing(true)
+        } else {
+            inSearch = true
+            let lowerCase = searchBar.text!.lowercased() // converting to lower case because csv contains names in lowercase
+            filteredPokemon = pokemon.filter({$0.name.range(of: lowerCase) != nil})
+            collection.reloadData()
+        }
+    }
+    
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
 }
 
